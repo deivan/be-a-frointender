8fba7ad277b9a2f5a9d893be5a56b8476dfc2a96
Ivan Demenkov <ariastation@ua.fm>
2023-09-23 16:50:55 +0300

    added lesson 03 code base

---
+++ app.js
@@ -0,0 +1,9 @@
+const API_USERS = 'https://jsonplaceholder.typicode.com/users';
+const API_ALBUMS = 'https://jsonplaceholder.typicode.com/albums';
+const API_PHOTOS = 'https://jsonplaceholder.typicode.com/photos';
+
+class ApiReader {
+    constructor() {
+        
+    }
+}
