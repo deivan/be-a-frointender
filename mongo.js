e62627ffafd501f201fc6b97cb53a8949dc4de46
Ivan Demenkov <ariastation@ua.fm>
2023-11-15 21:08:35 +0200

    added lesson

---
+++ mongo.js
@@ -0,0 +1,21 @@
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://tester:aamG5ZchmdxVKC9x@cluster0.j0oyw53.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  let db, arr = null;
+
+  try {
+    db = client.db("sample_mflix");
+    db.command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // arr = await db.listCollections().toArray();
+  } finally {
+    await client.close();
+  }
+  return arr;
+}
+
+module.exports = run;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const runMongo = require('../mongo');
 
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  if (req.session?.user) {
-    req.session.times++;
-  } else {
-    req.session.user = req.headers['user-agent'];
-    req.session.times = 1;
-  }
-  res.render('index', { title: 'Express', info: 'some info about smt',
-              user: req.session?.user, times: req.session?.times });
+  res.render('index', { title: 'Mongo DB demo' });
 });
 
-router.get('/hard', (req, res) => {
-  let a = 0;
-  for (let i = 0; i < 1e12; i++) {
-    a+=i;
-  }
-  res.send('operaion ended')
-})
+router.get('/mongo', function(req, res, next) {
+  runMongo()
+    .then(result => {
+      res.render('mongo', { result: JSON.stringify(result) });
+    })
+    .catch(err => {
+      res.render('mongo', { result: err });
+    });
+});
 
 module.exports = router;
