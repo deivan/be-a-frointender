2b091553a6edeff1e8a62b94f73e5a043ecc19bd
Ivan Demenkov <ariastation@ua.fm>
2023-11-19 19:04:36 +0200

    started postgress lesson

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const runMongo = require('../mongo');
+const secret = ['ihfoktij', 'qReAQK9hxHIRatjtu2f9pknnVQt1UKWp']
+const pg = require("pg");
+const client = new pg.Client(`postgres://${secret[0]}:${secret[1]}@ella.db.elephantsql.com/ihfoktij`);
+
+client.connect();
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mongo DB demo' });
 });
 
-router.get('/mongo', function(req, res, next) {
-  runMongo()
-    .then(result => {
-      res.render('mongo', { result: JSON.stringify(result) });
-    })
-    .catch(err => {
-      res.render('mongo', { result: err });
-    });
+router.get('/psql', async function(req, res, next) {
+  let data = null;
+  try {
+    //data = await client.query("SELECT * FROM users");
+    //console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+  res.render('psql', { data });
 });
 
 module.exports = router;
